8df0960a2a43436a740c873074a147a930f8559d
MarkoSukhyi-STUDENT <g00481928@atu.ie>
2026-09-21 12:58:41 +0100

    Added new method for display book details and added 2 objects

--- src/main/java/ie/atu/opp/week1/Book.java
+++ src/main/java/ie/atu/opp/week1/Book.java
@@ -4,6 +4,15 @@ public class Book
 {
     public String title;
     public String author;
-    public String pageCont;
-    public boolean available = true;
+    public int pageCont;
+    public boolean isAvailable = true;
+
+    public void InfoPrint()
+    {
+        System.out.println("Book: " + title);
+        System.out.println("Book Author: " + author);
+        System.out.println("Book Page: " + pageCont);
+        System.out.println("Is Book available? " + isAvailable);
+    }
 }
+
--- src/main/java/ie/atu/opp/week1/Main.java
+++ src/main/java/ie/atu/opp/week1/Main.java
@@ -7,5 +7,22 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Hello OPP");
+
+        Book firstBook = new Book();
+
+        firstBook.title = "Surrounded by Idiots";
+        firstBook.author = "Thomas Erikson";
+        firstBook.pageCont = 387;
+
+        firstBook.InfoPrint();
+
+        Book secondBook = new Book();
+
+        secondBook.title = "The Lord of the Rings";
+        secondBook.author = "J.K. Rowling";
+        secondBook.pageCont = 387;
+        secondBook.InfoPrint();
+
+        secondBook.InfoPrint();
     }
 }
